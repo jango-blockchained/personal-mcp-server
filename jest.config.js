20dc9eee0d2c4b56a83f4773ad4b506184cc3b09
Andi Ashari <a.ashari1302@gmail.com>
2025-03-17 15:27:18 +0700

    Add JSON summary coverage reporter to Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,18 @@
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-  transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        useESM: true,
-      },
-    ],
-  },
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '/src/utils/logger.util.ts'
-  ],
-}; 
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	extensionsToTreatAsEsm: ['.ts'],
+	moduleNameMapper: {
+		'^(\\.{1,2}/.*)\\.js$': '$1',
+	},
+	transform: {
+		'^.+\\.tsx?$': [
+			'ts-jest',
+			{
+				useESM: true,
+			},
+		],
+	},
+	coveragePathIgnorePatterns: ['/node_modules/', '/src/utils/logger.util.ts'],
+	coverageReporters: ['text', 'lcov', 'json-summary'],
+};
